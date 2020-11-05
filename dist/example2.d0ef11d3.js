f5f0b0846f5b7fd3b95bb604072969210ae0c420
myid-hana <sosqkrgksml@gmail.com>
2020-11-05 18:20:43 +0900

    Promise.all()

--- dist/example2.d0ef11d3.js
+++ dist/example2.d0ef11d3.js
@@ -140,6 +140,25 @@ Promise.reject();
 Promise.reject(new Error('reason')).then(function (error) {}).catch(function (error) {
   console.log(error);
 });
+/* 
+프로미스 객체 여러 개를 생성하여, 
+배열로 만들어 인자로 넣고 Promise.all 을 실행하면 
+배열의 모든 프로미스 객체들이 fulfilled 되었을 때, then 의 함수가 실행된다. 
+then 의 함수의 인자로 프로미스 객체들의 resolve 인자값을 배열로 돌려준다. 
+*/
+//Promise.all([프로미스 객체들]);
+
+function p(ms) {
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
+      resolve('hello');
+    }, ms);
+  });
+}
+
+Promise.all([p(1000), p(2000), p(3000), p(4000)]).then(function (messages) {
+  console.log(messages);
+});
 },{}],"../../../../usr/local/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
