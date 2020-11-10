c90eefee10273ac0da51c7d936f75d8536a5eda2
myid-hana <sosqkrgksml@gmail.com>
2020-11-10 11:51:27 +0900

    node ex 인터넷의 동작 방법

--- webserver.js
+++ webserver.js
@@ -3,9 +3,9 @@ const http = require('http');
 const hostname = '127.0.0.1';
 const port = 1337;
  
-http.createServer((req, res) => {
+http.createServer((req, res) => { //서버를 만든다.
  res.writeHead(200, { 'Content-Type': 'text/plain' });
- res.end('Hello World\n');
-}).listen(port, hostname, () => {
+ res.end('Hello World\n'); //응답이 끝나면 해당 문구를 출력한다.
+}).listen(port, hostname, () => { //만들어진 서버가 이 컴퓨터의 1337번 포트에 리스닝하도록 하고 ip 는 hostname 이다.
  console.log(`Server running at http://${hostname}:${port}/`);
 });
