20a75c664989020218a2a4b2b53b3d9971ba9026
myid-hana <sosqkrgksml@gmail.com>
2020-11-11 14:26:09 +0900

    node ex 템플릿엔진 사용법

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const express = require('express');
 let app = express(); //애플리케이션을 리턴한다. 
 
+app.set('view engine', 'pug'); //템플릿과 express 를 연결 
+app.set('views', './views'); //템플릿이 있는 디렉토리
+
 app.use(express.static('public')); //정적인 데이터가 위치할 디렉토리를 지정한다. 
+
+app.get('/template', (req, res) => { //views 내의  template 를 라우팅
+    res.render('temp') //temp 라는 template 파일은 웹페이지로 전송
+});
 
 //get 함수를 라우터라고 한다. 라우터는 사용자의 접속을 어떤 컨트롤러에 연결할 것인가 중개를 해주는 역할. 
 app.get('/', (req, res) => { //겟방식으로 홈에 접속한 사용자에게 처음 노출될 화면 
